0c2d69ccc60cf55a6206aef59bcb22c787f6a719
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-10-24 15:22:55 +0200

    zadanko1

--- src/main/scala/EShop/lab2/CartFSM.scala
+++ src/main/scala/EShop/lab2/CartFSM.scala
@@ -1,5 +1,6 @@
 package EShop.lab2
 
+import EShop.lab2.CartActor._
 import EShop.lab2.CartFSM.Status
 import akka.actor.{LoggingFSM, Props}
 
@@ -27,15 +28,39 @@ class CartFSM extends LoggingFSM[Status.Value, Cart] {
   startWith(Empty, Cart.empty)
 
   when(Empty) {
-    ???
+    case Event(AddItem(item), cart) => goto(NonEmpty).using(cart.addItem(item))
   }
 
   when(NonEmpty, stateTimeout = cartTimerDuration) {
-    ???
+    case Event(StateTimeout, _) =>
+      goto(Empty).using(Cart.empty)
+
+    case Event(AddItem(item), cart) =>
+      goto(NonEmpty).using(cart.addItem(item))
+
+    case Event(RemoveItem(item), cart) =>
+      if (cart.contains(item)) {
+        val newCart = cart.removeItem(item)
+        if (newCart.size == 0) {
+          goto(Empty).using(newCart)
+        } else {
+          goto(NonEmpty).using(newCart)
+        }
+      } else {
+        log.debug(s"Trying to remove non-existing item from cart: $item")
+        stay()
+      }
+
+    case Event(StartCheckout, cart) =>
+      goto(InCheckout).using(cart)
   }
 
   when(InCheckout) {
-    ???
+    case Event(CancelCheckout, cart) =>
+      goto(NonEmpty).using(cart)
+
+    case Event(CloseCheckout, _) =>
+      goto(Empty).using(Cart.empty)
   }
 
 }
